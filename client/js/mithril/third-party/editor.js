d074270941d4b81b99d3ccd418aef8aa200105d1
Hardware <contact@meshup.net>
2015-10-24 18:07:17 +0200

    fix(): correction l'event 'onkeydown' du textarea

--- client/js/mithril/third-party/editor.js
+++ client/js/mithril/third-party/editor.js
@@ -17,7 +17,7 @@ $(function(){
     e = e || event;
     if(e.keyCode === 13 && !document.getElementById('disable-enter-action').checked) {
       e.preventDefault();
-      if(textarea.value.length > 1)
+      if(textarea.value.length > 0)
         messages.vm.send();
       else
         textarea.value = null;
