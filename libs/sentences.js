9a87a68056144349624875b5194aa0d85d0edf6c
Hardware <contact@meshup.net>
2015-10-31 23:04:46 +0100

    add(): ajout de quelques phrases pour chatbot

--- libs/sentences.js
+++ libs/sentences.js
@@ -5,8 +5,31 @@ module.exports = {
       "%user peut gagner une partie de puissance 4 en trois coups",
       "rm -rf en cours sur le serveur de %user",
       "Activation de Skynet imminente...",
-      "Tu es mauvais, tu ne vaut rien %user",
-      "Ça va durer encore longtemps ?"
+      "Tu es mauvais, tu ne vaux rien %user",
+      "Chatbot joue du thérémine depuis l'âge de 3 ans",
+      "Ça va durer encore longtemps ?",
+      "Donne un coup de tatane à %user",
+      "Je suis le seul à pouvoir kick mes créateurs",
+      "Je parle couramment 137 dialectes, dont le klingon",
+      "Chatbot sait programmer en Malbolge depuis l'âge de 4 ans",
+      "Observe %user avec attention...",
+      "Tapis dans l'ombre, Chatbot rôde...",
+      "Je suis le seul à avoir survecu à la digestion du monstre des sables, le Sarlacc",
+      "Chatbot a déjà battu 3 fois le Général Grievous aux échecs",
+      "Kick de %user dans 30 secondes. Prépare toi au décollage, ça va secouer :noel:",
+      "Passage de %user au détecteur de mensonge",
+      "%user porte des jupes",
+      "Chatbot a déjà fait un échec et mat les yeux bandés à Kasparov en 3 coups",
+      "%user ne fait que mentir depuis le début",
+      "Une seule Tortue Ninja a osé défier Chatbot. Maintenant on l'appelle Franklin",
+      "Chatbot est inébranlable... oui personne ne peut le branler",
+      "Les seedbox c'est pour les peureux. Chatbot demande ce qu'il veut aux Majors",
+      "Je n'utilise pas le mode AFK vu je peux être à plusieurs endroits en même temps",
+      "Chatbot ne dort jamais",
+      "La prochaine fois que %user parle, c'est un ban définitif du chat",
+      "%user a toujours raison. Et s'il a tort, c'est qu'il a raison d'avoir tort.",
+      "Vous devriez vous méfier de %user, il fait des choses louches devant son écran",
+      "Une analyse de la NSA a été détectéé sur le chat, procédure de confinement en cours..."
     ]
   }
 };
