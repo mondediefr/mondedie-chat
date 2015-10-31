f827d975e2fc33d144e42071ba9f559aae3909ee
hydrog3n <loic.vaille@gmail.com>
2015-10-31 20:55:18 +0100

    add(sentences) : Ajout de deux phrases
    Premiere OSS 117 seconde Pub Spontex ;)

--- libs/sentences.js
+++ libs/sentences.js
@@ -4,7 +4,9 @@ module.exports = {
       "Démarrage de la procédure d'auto-destruction du chat",
       "%user peut gagner une partie de puissance 4 en trois coups",
       "rm -rf en cours sur le serveur de %user",
-      "Activation de Skynet imminente..."
+      "Activation de Skynet imminente...",
+      "Tu es mauvais, tu ne vaut rien %user",
+      "Ça va durer encore longtemps ?"
     ]
   }
 };
