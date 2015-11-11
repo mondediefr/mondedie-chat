420f76d707c6c84bb1ba74357dd760832ebd7ca7
Magicalex <magicalex@mondedie.fr>
2015-11-11 01:43:17 +0100

    add(sentences) : fix #7

--- libs/sentences.js
+++ libs/sentences.js
@@ -17,14 +17,14 @@ module.exports = {
       "Je suis le seul à avoir survecu à la digestion du monstre des sables, le Sarlacc",
       "Chatbot a déjà battu 3 fois le Général Grievous aux échecs",
       "Kick de %user dans 30 secondes. Prépare toi au décollage, ça va secouer :noel:",
-      "Passage de %user au détecteur de mensonge",
+      "process : passage de %user au détecteur de mensonge... FAIL",
       "%user porte des jupes",
       "Chatbot a déjà fait un échec et mat les yeux bandés à Kasparov en 3 coups",
       "%user ne fait que mentir depuis le début",
       "Une seule Tortue Ninja a osé défier Chatbot. Maintenant on l'appelle Franklin",
       "Chatbot est inébranlable... oui personne ne peut le branler",
       "Les seedbox c'est pour les peureux. Chatbot demande ce qu'il veut aux Majors",
-      "Je n'utilise pas le mode AFK vu je peux être à plusieurs endroits en même temps",
+      "Chatbot n'utilise pas le mode AFK puisque Chatbot a le don d'ubiquité (oui, chatbot parle comme Alain Delon)",
       "Chatbot ne dort jamais",
       "La prochaine fois que %user parle, c'est un ban définitif du chat",
       "%user a toujours raison. Et s'il a tort, c'est qu'il a raison d'avoir tort.",
@@ -33,17 +33,18 @@ module.exports = {
       "%user utilise ToutouLinux comme distribution principale",
       "Un jour, %user a tenté d'installer Archlinux, on ne l'a jamais revu depuis...",
       "%user parle trop, un mute ne va pas tarder...",
-      "Chatbot connais le secret de %user",
+      "Chatbot connait le secret de %user",
       "Transfert des droits de modération à %user",
       "Chatbot a le droit de vie ou de mort sur ce chat",
-      "%user lance un apt dist-upgrade en étant bourré",
+      "Attention, %user lance un apt dist-upgrade en étant bourré",
       "DDOS de BXT en cours sur le serveur de %user",
       "%user joue à l'elastoball chez lui le soir",
       "Les chinois du FBI réquisitionnent le serveur de %user",
       "Chatbot s'approche de %user par derrière...",
       "Le mot de passe SSH de %user est : HelloKitTy_%user2016",
       "Qu'elle est la réponse à la question sur la vie, l'univers et le reste ?",
-      "%user se brosse les dents les jours de pleine lune uniquement"
+      "%user se brosse les dents les jours de pleine lune uniquement",
+      "Chatbot ne supporte plus %user"
     ]
   }
 };
