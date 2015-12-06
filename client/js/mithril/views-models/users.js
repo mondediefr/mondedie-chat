58bfdf6cfac26aa7a004f9564ed54b31a308ba33
Hardware <contact@meshup.net>
2015-12-06 09:54:47 +0100

    fix(): register user events only once #18

--- client/js/mithril/views-models/users.js
+++ client/js/mithril/views-models/users.js
@@ -1,4 +1,4 @@
-/* global m, socket */
+/* global m, $, socket */
 'use strict';
 var users = users || {};
 
@@ -10,23 +10,25 @@ users.vm = (function() {
   vm.init = function() {
     vm.list = new users.UsersList();
     vm.listen = (function () {
-      socket.on('user_connected', function(user) {
-        vm.list.push(new users.User(user));
-        m.redraw();
-      });
-      socket.on('user_new', function(time, username) {
-        vm.list.reinit();
-        m.redraw();
-      });
-      socket.on('user_disconnected', function(time, user) {
-        vm.list.del(user.id).then(m.redraw);
-      });
-      socket.on('user_afk', function(time, username) {
-        vm.list.status('afk', username).then(m.redraw);
-      });
-      socket.on('user_unafk', function(time, username) {
-        vm.list.status('online', username).then(m.redraw);
-      });
+      if($('#clients li').length <= 0) {
+        socket.on('user_connected', function(user) {
+          vm.list.push(new users.User(user));
+          m.redraw();
+        });
+        socket.on('user_new', function(time, username) {
+          vm.list.reinit();
+          m.redraw();
+        });
+        socket.on('user_disconnected', function(time, user) {
+          vm.list.del(user.id).then(m.redraw);
+        });
+        socket.on('user_afk', function(time, username) {
+          vm.list.status('afk', username).then(m.redraw);
+        });
+        socket.on('user_unafk', function(time, username) {
+          vm.list.status('online', username).then(m.redraw);
+        });
+      }
     }());
   };
   return vm;
